09f9894b21384672c6012fa925bff033db994f23
Paul OBrien <PVOVideo@outlook.com>
2020-10-19 23:15:02 -0700

    save point

--- app/src/main/java/com/pvobrien/github/taskmaster/AddTask.java
+++ app/src/main/java/com/pvobrien/github/taskmaster/AddTask.java
@@ -1,22 +1,37 @@
 package com.pvobrien.github.taskmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddTask extends AppCompatActivity{
+public class AddTask extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_task);
-    }
 
-    @Override
-    public void onClick(View view) {
-        EditText itemTask
-    }
 
+        Button addButton = AddTask.this.findViewById(R.id.addTaskButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText taskName = AddTask.this.findViewById(R.id.taskName);
+                EditText taskDetails = AddTask.this.findViewById(R.id.taskDetails);
+                String newTaskName = taskName.getText().toString();
+                String newTaskDetails = taskDetails.getText().toString();
+                System.out.println(String.format("You have a new task called %s and you'll be %s all days long", newTaskName, newTaskDetails));
+
+                Intent addTaskToAllTasks = new Intent(AddTask.this, AllTasks.class);
+                addTaskToAllTasks.putExtra("name", newTaskName);
+                addTaskToAllTasks.putExtra("details", newTaskDetails);
+                AddTask.this.startActivity(addTaskToAllTasks);
+            }
+        });
+    }
 }
