df2cb5bf9e03d81b4e383dfb93e6ba062895dbb1
Paul OBrien <PVOVideo@outlook.com>
2020-10-19 20:46:08 -0700

    correct refresh

---
+++ app/src/main/java/com/pvobrien/github/taskmaster/AddTask.java
@@ -0,0 +1,22 @@
+package com.pvobrien.github.taskmaster;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTask extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText itemTask
+    }
+
+}
