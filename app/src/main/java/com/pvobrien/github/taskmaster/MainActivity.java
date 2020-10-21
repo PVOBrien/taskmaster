beaabb54d0cdb3bf3ca58aff41a7f35741c1b965
Paul OBrien <PVOVideo@outlook.com>
2020-10-20 20:54:56 -0700

    complete Settings Username handoff

--- app/src/main/java/com/pvobrien/github/taskmaster/MainActivity.java
+++ app/src/main/java/com/pvobrien/github/taskmaster/MainActivity.java
@@ -2,11 +2,25 @@ package com.pvobrien.github.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView myTaskTitle = findViewById(R.id.myTasksTitle);
+        String greeting = String.format("%s's tasks", preferences.getString("savedUsername", "My Tasks"));
+        myTaskTitle.setText(greeting);
+        SharedPreferences.Editor preferenceEditor = preferences.edit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +44,12 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.this.startActivity(goToAllTasksNow);
     });
 
+
+    ImageButton goToSettings = MainActivity.this.findViewById(R.id.homeToSettingsButton);
+    goToSettings.setOnClickListener((view) -> {
+        System.out.println("heading to settings.");
+        Intent goToSettingPage = new Intent (MainActivity.this, Settings.class);
+        MainActivity.this.startActivity(goToSettingPage);
+    });
     }
 }
---
+++ app/src/main/java/com/pvobrien/github/taskmaster/Settings.java
@@ -0,0 +1,36 @@
+package com.pvobrien.github.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+            // TODO: add Support ActionBar
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences.Editor preferenceEditor = preferences.edit();
+
+        findViewById(R.id.saveSettingButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username = findViewById(R.id.usernameText);
+                preferenceEditor.putString("savedUsername", username.getText().toString());
+                preferenceEditor.apply();
+            }
+        });
+    }
+
+    //TODO: add Support ActionBar
+
+}
